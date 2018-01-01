308f409e1eb2431a552adac569353d46aca39904
kast <mariella.kast@epfl.ch>
2018-01-01 13:26:47 +0100

    Changed initial 4

--- Project3/Project3/initial_4_ex.m
+++ Project3/Project3/initial_4_ex.m
@@ -5,11 +5,11 @@ x_left = x-0.5*h;
 x_right = x+0.5*h;
 h_0 = ones(length(x),1);
 m_0 = ones(length(x),1)*(-1.5);
-for i=1:length(x)-1
+for i=1:length(x)
     if (x_left(i) >1)
         m_0(i)=0;
     elseif ( x_right(i) >1)
-        m_0(i) = (1-x_right(i))*(-1.5);
+        m_0(i) = (x_right(i)-1)*(-1.5);
     end
 end
 end
